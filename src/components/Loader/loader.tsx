8ff1e770a80adb8f86c19c92a9cadfb60994b6d8
Jose Antonio Martin Alfonso <jmartalf@emeal.nttdata.com>
2024-09-07 12:42:05 +0200

    comit upercaser folder name after set git config core.ignorecase false

---
+++ src/components/Loader/loader.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import '../../styles/loader.css'
+
+export const Loader: React.FC = () => {
+    return (
+        <>
+            <div className="loader" data-testid="loader"></div>
+        </>
+    )
+}
